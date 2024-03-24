a8165255a085ed1c834efa6645362bd1b9567347
ABIN-VARGHEXE <abinvarghese092005@gmail.com>
2024-03-25 02:00:49 +0530

    name

--- src/components/MobileNav/MobileNav.jsx
+++ src/components/MobileNav/MobileNav.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
 import "./MobileNav.css";
 const handleMailClick = () => {
   // Open the default mail client with the specified email address
-  window.open('mailto:abinvarghese092005@gmail.com');
+  window.open("mailto:abinvarghese092005@gmail.com");
 };
 const MobileNav = ({ isOpen, toggleMenu }) => {
   return (
@@ -14,21 +14,49 @@ const MobileNav = ({ isOpen, toggleMenu }) => {
       >
         <div className="mobile-menu-container">
           <h2 className="logo" alt="">
-            LOGO.
+            ABIN.
           </h2>
           <ul>
-          <li>
-                            <Link className="menu-item" to="home" smooth={true} duration={500}>Home</Link>
-                        </li>
-                        <li>
-                            <Link className="menu-item" to="skills" smooth={true} duration={500}>Skills</Link>
-                        </li>
-                        <li>
-                            <Link className="menu-item" to="projects" smooth={true} duration={500}>Projects</Link>
-                        </li>
-                        <li>
-                            <Link className="menu-item" to="contact" smooth={true} duration={500}>Contact</Link>
-                        </li>
+            <li>
+              <Link
+                className="menu-item"
+                to="home"
+                smooth={true}
+                duration={500}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="menu-item"
+                to="skills"
+                smooth={true}
+                duration={500}
+              >
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="menu-item"
+                to="projects"
+                smooth={true}
+                duration={500}
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="menu-item"
+                to="contact"
+                smooth={true}
+                duration={500}
+              >
+                Contact
+              </Link>
+            </li>
 
             <button className="contact-btn" onClick={handleMailClick}>
               Hire Me
