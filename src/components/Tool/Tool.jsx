b4611a577ece5cbb50f6be2165e6917ee559238f
ABIN VARGHEXE <abinvarghese092005@gmail.com>
2024-06-20 01:41:08 +0530

    chore: Update react-pdf dependency to version 9.0.0

--- src/components/Tool/Tool.jsx
+++ src/components/Tool/Tool.jsx
@@ -4,19 +4,25 @@ import { ICONS } from "../../../public/data/data";
 import { Fade } from "react-awesome-reveal";
 const Tool = () => {
   return (
-    
     <div className="tool-main">
       <p className="tool-tera">Tools Known</p>
       <div className="tool-sub">
         <div className="img-main">
-                <Fade direction='up' cascade damping={0.4} delay={650} triggerOnce={true} duration={850}>
-                  {ICONS.map(({ id, src, title, style }) => (
-                    <div key={id} className={`icon ${style} img-primary`}>
-                      <img className="img-box" src={src} alt={title} />
-                      <p className="img-text">{title}</p>
-                    </div>
-                  ))}
-                </Fade>
+          <Fade
+            direction="up"
+            cascade
+            damping={0.4}
+            delay={250}
+            triggerOnce={true}
+            duration={550}
+          >
+            {ICONS.map(({ id, src, title, style }) => (
+              <div key={id} className={`icon ${style} img-primary`}>
+                <img className="img-box" src={src} alt={title} />
+                <p className="img-text">{title}</p>
+              </div>
+            ))}
+          </Fade>
         </div>
       </div>
     </div>
